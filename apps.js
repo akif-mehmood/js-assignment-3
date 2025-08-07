e8359944cb901f782132fc960c6f3c95f272823e
akif-mehmood <muhammadakif148@gmail.com>
2025-08-07 17:19:01 +0500

    3rd assignment update

---
+++ apps.js
@@ -0,0 +1,24 @@
+// var age =17
+// alert(`I am ${age} years old`)
+// var visit =14
+// alert(`You have visited this site ${visit} times`)
+// var birthyear = 2008
+// document.writeln(`My birth year is ${birthyear}<br>`)
+// var name ="shakeel bhai"
+// var product = "T-shirts"
+// var quantity =5
+// document.writeln(`${name} ordered ${quantity} ${product} on XYZ store`)
+
+var age = 27
+alert(`I am ${age} years old`)
+
+var visit = 20 
+alert(`I have visited this site ${visit} thimes`)
+
+var birthYear = 1998
+document.writeln(`My Birth Year is ${birthYear}<br>`)
+
+var name = "Akif Mehmood"
+var star = "Scorpion"
+var year = "26"
+document.writeln(`My name is ${name}, My star sign is ${star}.. I am now ${year} years old.`)
